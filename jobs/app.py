3d4b531928d95e05a29e421894e877087da308af
tipstein <adam.w.stein@gmail.com>
2023-01-16 12:30:29 -0700

    update correct code

--- jobs/app.py
+++ jobs/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-def my_decorator(route):
-    def jobs():
-        return render_template( index.html )  
-    route(jobs + "/")
+@app.route('/')
+@app.route('/jobs')
+def jobs():
+   return render_template('index.html')
+
